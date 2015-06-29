963f1d2ea8dd5e77a6c85c57c29fb0b7be245513
Angelo Olivera <aolivera@gmail.com>
2015-06-29 10:34:45 -0400

    fix path order

--- 00_path.zsh
+++ 00_path.zsh
@@ -5,4 +5,7 @@ fi
 
 PATH=/usr/local/bin:$PATH
 
-path=(~/bin /usr/local/sbin /usr/local/bin $path /sbin /usr/sbin)
+path=(/opt/chefdk/bin ~/bin ~/src/go/bin/ /usr/local/sbin /usr/local/bin $path /sbin /usr/sbin)
+path+=(/Users/redondos/Downloads/AWS-ElasticBeanstalk-CLI-2.6.3/eb/macosx/python2.7)
+path+=(/Users/redondos/Library/Python/2.7/bin)
+path+=(/Users/redondos/.chefdk/gem/ruby/2.1.0/bin)
