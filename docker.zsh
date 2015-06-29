4d980ce3bd77643f9ca6278bde991b141b835ebc
Angelo Olivera <aolivera@gmail.com>
2015-06-29 10:28:01 -0400

    configure docker

--- docker.zsh
+++ docker.zsh
@@ -1,5 +1,7 @@
 if [[ $(uname -s) == "Darwin" ]]; then
-  export DOCKER_HOST=tcp://localhost:4243
-  export DOCKER_HOST=tcp://192.168.59.103:2375
+    export DOCKER_HOST=tcp://localhost:4243
+    export DOCKER_HOST=tcp://192.168.59.103:2376
+    export DOCKER_CERT_PATH=/Users/redondos/.boot2docker/certs/boot2docker-vm
+    export DOCKER_TLS_VERIFY=1
 fi
 
