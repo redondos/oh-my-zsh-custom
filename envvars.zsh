cbae01dd16dceac7ed634edb19f22faef76df6dd
Angelo Olivera <aolivera@gmail.com>
2020-02-11 19:49:18 -0500

    add github oauth token

--- envvars.zsh
+++ envvars.zsh
@@ -1,3 +1,5 @@
 export EDITOR=vim
 export GOPATH=~/src/golang
 export HOMEBREW_NO_INSTALL_CLEANUP=1
+
+export GITHUB_OAUTH_TOKEN=$(/usr/bin/security -q find-internet-password -g -w -s github-oauth-token -a redondos)
